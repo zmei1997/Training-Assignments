d1e51c8ad0708434c4c49e22773db1d538b71a96
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-05-04 20:04:21 -0400

    upload assignment1

---
+++ Zhongxiao_Mei_Solution_for_assignment1.sql
@@ -0,0 +1,175 @@
+use AdventureWorks2017
+go
+
+/*
+	question 1
+*/
+select	ProductID, [Name], Color, ListPrice
+from	Production.Product
+;
+
+/*
+	question 2
+*/
+select	ProductID, [Name], Color, ListPrice
+from	Production.Product
+where	ListPrice = 0
+;
+
+/*
+	question 3
+*/
+select	ProductID, [Name], Color, ListPrice
+from	Production.Product
+where	Color is NULL
+;
+
+/*
+	question 4
+*/
+select	ProductID, [Name], Color, ListPrice
+from	Production.Product
+where	Color is not NULL
+;
+
+/*
+	question 5
+*/
+select	ProductID, [Name], Color, ListPrice
+from	Production.Product
+where	Color is not NULL
+		and
+		ListPrice > 0
+;
+
+/*
+	question 6
+*/
+select	CONCAT([Name], Color) as Report
+from	Production.Product
+where	Color is not NULL
+;
+
+/*
+	question 7
+*/
+select	'NAME: ' + [Name] + ' -- ' + 'COLOR: ' + Color as [Name And Color]
+from	Production.Product
+where	Color is not NULL
+;
+
+/*
+	question 8
+*/
+select	ProductID, [Name]
+from	Production.Product
+where	ProductID between 400 and 500
+;
+
+/*
+	question 9
+*/
+select	ProductID, [Name], Color
+from	Production.Product
+where	Color in ('black', 'blue')
+;
+
+/*
+	question 10
+*/
+select	[Name] as Report
+from	Production.Product
+where	[Name] like 'S%'
+;
+
+/*
+	question 11
+*/
+select	[Name], ListPrice
+from	Production.Product
+where	[Name] like 'S%'
+order by	[Name] asc
+;
+
+/*
+	question 12
+*/
+select	[Name], ListPrice
+from	Production.Product
+where	[Name] like '[A,S]%'
+order by	[Name] asc
+;
+
+/*
+	question 13
+*/
+select	[Name]
+from	Production.Product
+where	[Name] like 'SPO[^K]%'
+order by	[Name]
+;
+
+/*
+	question 14
+*/
+select	distinct Color as [unique colors]
+from	Production.Product
+--where	Color is not NULL
+order by	Color desc
+;
+
+/*
+	question 15
+*/
+select	ProductSubcategoryID, Color
+from	Production.Product
+where	ProductSubcategoryID is not NULL
+		and Color is not NULL
+group by	ProductSubcategoryID, Color
+order by	ProductSubcategoryID, Color
+;
+
+/*
+	question 16
+*/
+-- solution that only includes products in red and black
+SELECT ProductSubCategoryID
+      , LEFT([Name],35) AS [Name]
+      , Color, ListPrice 
+FROM Production.Product
+WHERE (Color IN ('Red','Black') 
+	  AND ListPrice BETWEEN 1000 AND 2000 
+	  AND ProductSubCategoryID <> 1)
+	  OR
+	  (Color IN ('Red','Black') AND ProductSubCategoryID = 1)
+ORDER BY ProductID
+;
+
+-- solution that includes products in other colors (not only red and black)
+SELECT ProductSubCategoryID
+      , LEFT([Name],35) AS [Name]
+      , Color, ListPrice 
+FROM Production.Product
+WHERE (Color IN ('Red','Black') 
+	  AND ListPrice BETWEEN 1000 AND 2000 
+	  AND ProductSubCategoryID <> 1)
+	  OR
+	  (Color IN ('Red','Black') AND ProductSubCategoryID = 1)
+	  OR
+	  (Color not IN ('Red','Black'))
+ORDER BY ProductID
+;
+
+/*
+	question 17
+*/
+select	ProductSubcategoryID, [Name], Color, ListPrice
+from	Production.Product
+where	[Name] like 'Mountain-500 Black%'
+		or [Name] like 'Road-350-W Yellow%'
+		or [Name] like 'HL Mountain Frame - Silver%'
+		or [Name] like 'HL Road Frame - Red%'
+		or [Name] like 'HL Road Frame - Black%'
+		and ProductSubcategoryID is not NULL
+		and Color is not NULL
+;
