da2ff36065931796260edf2f2a944ab121a8ec2c
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-06-02 20:36:29 -0400

    uploaded day20 materials

---
+++ Day20/MovieShop/MovieShop.API/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PostMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMovie()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IActionResult GetPurchases()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Day20/MovieShop/MovieShop.MVC/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
