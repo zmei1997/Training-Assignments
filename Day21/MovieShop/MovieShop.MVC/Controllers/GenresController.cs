792ec394d06338a0e62302bd1c3b2e8feb18f0a5
zmei1997 <68665894+zmei1997@users.noreply.github.com>
2021-06-03 21:20:02 -0400

    upload day21 materials

---
+++ Day21/MovieShop/MovieShop.MVC/Controllers/GenresController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.ServiceInterfaces;
+
+namespace MovieShop.MVC.Controllers
+{
+    public class GenresController : Controller
+    {
+        private readonly IGenreService _genreService;
+        public GenresController(IGenreService genreService)
+        {
+            _genreService = genreService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day21/MovieShop/MovieShop.MVC/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.ServiceInterfaces;
+
+namespace MovieShop.MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult Purchase()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Favorite()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Unfavorite()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Review()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movie()
+        {
+            return View();
+        }
+    }
+}
